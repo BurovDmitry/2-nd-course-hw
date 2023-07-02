492c4202d15226c55a834cfa8eb5a3c574948809
Burov Dmitry <burovdo88@gmail.com>
2023-07-02 23:34:48 +0500

    2.3

---
+++ hw-2.3.js
@@ -0,0 +1,91 @@
+// Задание 1
+
+let password = String(prompt(`Введите пароль (12345)`));
+if (password === '12345') {
+    console.log('№1 Пароль введен верно');
+} else {
+    console.log('№1 Пароль введен неправильно');
+}
+
+// Задание 2
+
+let c = 4
+if (c >= 0 && c <=10) {
+    console.log('№2 Верно')
+} else {
+    console.log('№2 Неверно')
+ }
+
+// Задание 3
+
+let d = 2;
+let e = 101;
+if (d >=100 || e >=100) {
+    console.log('№3 Верно')
+} else {
+    console.log('№3 Неверно')
+}
+
+// Задание 4
+
+let a = '2';
+let b = '3';
+a = Number(a);
+b = Number(b);
+alert (a + b);
+
+// Задание 5
+
+let monthNumber = String(prompt(`Введите номер месяца`));
+
+switch (monthNumber) {
+    case '12':
+    case '1':
+    case '2':    
+        console.log('№4 Зима')
+        break;
+    case '3':
+    case '4':
+    case '5':
+        console.log('№4 Весна')
+        break;
+    case '6':
+    case '7':
+    case '8':
+        console.log('№4 Лето')
+        break;
+    case '9':
+    case '10':
+    case '11':
+        console.log('№4 Осень')
+        break;
+    default:
+        console.log('№4 Месяцев всего 12!!!')
+        break;
+}
+
+// Задание 7
+
+let numeric = Number(prompt(`Пожалуйста, введите любое число`));
+if (!isNaN(numeric)) {
+    if (numeric % 2 === 0) {
+        alert ('№7 Число четное');
+    }  else {
+        alert ('№7 Число не четное');
+    }
+} else {
+    console.log('NaN');
+}
+
+// Задание 8
+
+let clientOS = 0
+
+if (clientOS = 0) {
+    alert ('Установите версию приложения для iOS по ссылке');
+} else {
+    alert ('Установите версию приложения для Android по ссылке');
+} 
+
+// Задание 9
+// у меня уже было поздно, доделаю его попозже =(
